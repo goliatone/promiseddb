e38ecbacc709b1b30e50bdf524d3fa90ce93b76d
goliatone <burgosemi@gmail.com>
2014-09-29 00:20:02 -0400

    EXAMPLE: Added persistence example.

--- examples/main.js
+++ examples/main.js
@@ -10,21 +10,73 @@ requirejs.config({
 
 define(['promiseddb', 'jquery'], function (PromisedDB, $) {
     console.log('Loading');
+
+    var User = {
+    	storeName:'User',
+        add: function(user) {
+            return this.db.with(this.storeName, function(execute) {
+                this.objectStore('User')
+                    .put(user);
+            });
+        },
+        get: function(id) {
+            return this.db.with(this.storeName, function(execute) {
+                execute(this.objectStore('User')
+                    .get(id));
+            });
+        },
+        del: function(id) {
+            return this.db.with(this.storeName, function(execute) {
+                this.objectStore('User')
+                    .delete(id);
+            });
+        },
+        getUserStatus: function(id) {
+            var q = this.db.with(this.storeName, function(execute) {
+                execute(this.objectStore('User').get(id), 'user');
+            });
+
+            q.then(function(data) {
+                var user = data.user,
+                    active = user.active ? 'very active' : 'not very active';
+                console.debug(user.name + ' is ' + active);
+            }).catch(function(e) {
+                console.error('ERROR:', e);
+            });
+        }
+    };
+
 	var promiseddb = new PromisedDB({
 		version:1,
 		database:'testing',
 		defineSchema: function(){
 			console.log('DEFINE SCHEMA');
-			this.createObjectStore('People', {
+			this.createObjectStore('User', {
                 keyPath: 'id',
                 autoIncrement: true
             });
-            this.createObjectStore('Groups', {
+            this.createObjectStore('Group', {
                 keyPath: 'id',
                 autoIncrement: true
             });
 		}
 	});
+
+	User.db = promiseddb;
+
+	User.add({
+		id:1,
+        name: 'Pepe',
+        active: false
+    });
+
+    User.add({
+		id:2,
+        name: 'Rone',
+        active: true
+    });
+
+    User.getUserStatus(1);
 
 	window.db = promiseddb;
 });
